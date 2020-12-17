b5c739cece6fab5f1916c69bdae93e2edf16c705
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2020-12-17 16:25:51 +0530

    added key inputs

--- index-file.js
+++ index-file.js
@@ -25,7 +25,7 @@ function printResults(num) {
 }
 
 function getFormattedNumber(num) {
-  if(num=="-"){
+  if (num == "-") {
     return "";
   }
   var n = Number(num);
@@ -47,27 +47,31 @@ for (var i = 0; i < operators.length; i++) {
         output = output.substr(0, output.length - 1);
         printResults(output);
       }
-    } else {
+    } else if(this.id=="invertsign"){
+      var output = reverseNumberFormat(getResults());
+      output=output*-1;
+      printResults(output);
+    }else {
       var output = getResults();
       var history = getHistory();
-      if (output=="" && history!=""){
-        if (isNaN(history[history.length-1])){
-          history=history.substr(0, history.length-1);
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
         }
       }
-      if(output!="" || history!=""){
+      if (output != "" || history != "") {
         //conditional statement
-          output = output==""?output:reverseNumberFormat(output);
-          history = history + output;
-          if (this.id == "=") {
-            var res = eval(history);
-            printResults(res);
-            printHistory("");
-          } else {
-            history = history + this.id;
-            printHistory(history);
-            printResults("");
-          }
+        output = output == "" ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == "=") {
+          var res = eval(history);
+          printResults(res);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printResults("");
+        }
       }
     }
   });
@@ -81,3 +85,49 @@ for (var i = 0; i < numbers.length; i++) {
     }
   });
 }
+window.addEventListener('keydown', (event) => {
+  var key = event.key;
+  var output = getResults();
+  var history = getHistory();
+  if (key == "Escape") {
+    printHistory("");
+    printResults("");
+  } else if (key == "Backspace") {
+    var output = reverseNumberFormat(getResults()).toString();
+    if (output) {
+      output = output.substr(0, output.length - 1);
+      printResults(output);
+    }
+  } else if (key == "0" || key == "1" || key == "2" || key == "3" || key == "4" || key == "5" || key == "6" || key == "7" || key == "8" || key == "9") {
+    var out = reverseNumberFormat(getResults());
+    if (out != NaN) { //if output is a number
+      out = out + key;
+      printResults(out);
+    }
+  } else {
+    //conditional statement
+    output = output == "" ? output : reverseNumberFormat(output);
+    console.log(output);
+    history = history + output;
+    if (key == "Enter") {
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history +=0 ;
+        }
+      }
+      var res = eval(history);
+      printResults(res);
+      printHistory("");
+    } else if (key == "/" || key == "*" || key == "+" || key == "-") {
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
+        }
+      }else if(history!=""){
+        history = history + key;
+      }
+      printHistory(history);
+      printResults("");
+    }
+  }
+});
